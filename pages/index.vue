43439a9e386a9ed7070e6010b403155f62f78f81
Patrik Vaďura <patrikvadura@icloud.com>
2020-08-13 15:49:23 +0200

    first init

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,8 @@
 </template>
 
 <script>
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
+import Header from '~/components/header/Header'
+import Footer from '~/components/footer/Footer'
 export default {
   components: {
     Header,
